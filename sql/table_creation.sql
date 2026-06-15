43ee2dd5204ad78269397a499cae42e2aa2ea900
Sagar Joshi <128346886+TechSagarJoshi@users.noreply.github.com>
2026-06-15 08:55:12 +0530

    Create RetailSales table for sales data

---
+++ sql/table_creation.sql
@@ -0,0 +1,15 @@
+CREATE TABLE RetailSales (
+    InvoiceNo VARCHAR(50),
+    StockCode VARCHAR(50),
+    Description VARCHAR(255),
+    Quantity INT,
+    InvoiceDate DATETIME,
+    UnitPrice FLOAT,
+    CustomerID INT,
+    Country VARCHAR(100),
+    Revenue FLOAT,
+    Year INT,
+    Month INT,
+    Day INT,
+    Hour INT
+);
